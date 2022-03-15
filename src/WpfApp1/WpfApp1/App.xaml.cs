d47992e38578658d5abaeec180bb492cd46d792d
hosoya <hosoya@sp-inc.co.jp>
2022-03-15 11:41:25 +0900

    初回

---
+++ src/WpfApp1/WpfApp1/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Unity;
+using WpfApp1.Models;
+using WpfApp1.Views;
+using WpfApp1.ViewModels;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var container = new UnityContainer();
+            container.RegisterSingleton<ICalcService, CalcService>();
+
+            var vm = container.Resolve<MainWindowViewModel>();
+            new MainWindow(vm).Show();
+        }
+    }
+}
---
+++ src/WpfApp1/WpfApp1/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Reactive.Bindings;
+using System.Linq;
+using System.Reactive.Linq;
+using WpfApp1.Models;
+
+namespace WpfApp1.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveProperty<int> A { get; }
+        public ReactiveProperty<int> B { get; }
+        public ReactiveProperty<int> Result { get; }
+
+        public MainWindowViewModel(ICalcService calcService)
+        {
+            A = new ReactiveProperty<int>();
+            B = new ReactiveProperty<int>();
+            Result = ObservableEx.CombineLatest(A, B)
+                .Select(x => calcService.Sum(x.First, x.Second))
+                .ToReactiveProperty();
+        }
+    }
+}
